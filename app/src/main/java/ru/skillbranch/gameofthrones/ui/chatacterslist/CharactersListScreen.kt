c6794892af5078c1bbd5844f77a58c7ab948d372
Andrey Chesnovitskiy <a.chesnovitskiy@gmail.com>
2020-03-11 17:26:43 +0300

    continue header's animation

--- app/src/main/java/ru/skillbranch/gameofthrones/ui/chatacterslist/CharactersListScreen.kt
+++ app/src/main/java/ru/skillbranch/gameofthrones/ui/chatacterslist/CharactersListScreen.kt
@@ -3,9 +3,12 @@ package ru.skillbranch.gameofthrones.ui.chatacterslist
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.screen_characters_list.*
@@ -17,10 +20,14 @@ import ru.skillbranch.gameofthrones.utils.extensions.toShortName
 
 // TODO add comments ih whole project
 
-// TODO CharactersList header animation
+// TODO color of search karetka
 
 class CharactersListScreen : AppCompatActivity() {
-    var houseColor = Color.WHITE
+    companion object {
+        private const val ANIMATION_DURATION = 500L
+    }
+
+    private var houseColor = Color.WHITE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,22 +44,10 @@ class CharactersListScreen : AppCompatActivity() {
     private fun initViews() {
         view_pager_list.adapter = CharactersListFragmentAdapter(this)
 
-        val revealAnimation = AlphaAnimation(0f, 1f).apply {
-            duration = 1000L
-            interpolator = DecelerateInterpolator()
-            setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationRepeat(animation: Animation?) {
-                }
-
-                override fun onAnimationEnd(animation: Animation?) {
-                    app_bar.setBackgroundColor(houseColor)
-                }
-
-                override fun onAnimationStart(animation: Animation?) {
-                }
-
-            })
-        }
+        // Set tabs' text
+        TabLayoutMediator(tab_layout_houses, view_pager_list) { tab, position ->
+            tab.text = NEED_HOUSES[position].toShortName()
+        }.attach()
 
         // Set header's color when change page
         view_pager_list.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
@@ -61,14 +56,31 @@ class CharactersListScreen : AppCompatActivity() {
 
                 houseColor = HouseUtils.getPrimaryColor(NEED_HOUSES[position].toShortName())
 
-                view_reveal.setBackgroundColor(houseColor)
-                view_reveal.startAnimation(revealAnimation)
+                setHeaderAnimation()
             }
         })
+    }
 
-        // Set tabs' text
-        TabLayoutMediator(tab_layout_houses, view_pager_list) { tab, position ->
-            tab.text = NEED_HOUSES[position].toShortName()
-        }.attach()
+    private fun setHeaderAnimation() {
+        // TODO
+        val centerX = app_bar.width / 2
+        val centerY = app_bar.height
+        val endRadius = app_bar.width / 2F
+
+        val revealAnimation =
+            ViewAnimationUtils.createCircularReveal(view_reveal, centerX, centerY, 0F, endRadius)
+                .apply {
+                    duration = ANIMATION_DURATION
+                    interpolator = DecelerateInterpolator()
+                    doOnEnd {
+                        view_reveal.visibility = View.INVISIBLE
+                        app_bar.setBackgroundColor(houseColor)
+                    }
+                }
+
+        view_reveal.setBackgroundColor(houseColor)
+        view_reveal.visibility = View.VISIBLE
+
+        revealAnimation.start()
     }
 }
